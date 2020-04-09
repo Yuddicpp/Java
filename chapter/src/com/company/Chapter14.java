6d073f3c03997f3b129af5ce9800f081902e0065
Yuddicpp <1652534962@qq.com>
2020-04-09 21:28:17 +0800

    Chapter15

--- chapter/src/com/company/Chapter14.java
+++ chapter/src/com/company/Chapter14.java
@@ -1,7 +1,50 @@
 package com.company;
 
-public class Chapter14 {
-    public static void main(String[] args) {
-        
+import javafx.application.Application;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.stage.*;
+import java.awt.*;
+
+
+
+
+public class Chapter14 extends Application {
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Pane pane = new Pane();
+
+
+        Circle circle = new Circle();
+        circle.centerXProperty().bind(pane.widthProperty().divide(2));
+        circle.centerYProperty().bind(pane.heightProperty().divide(2));
+
+
+        circle.setStroke(Color.RED);
+        circle.setFill(Color.PURPLE);
+
+        Scene scene = new Scene(pane,700,500);
+
+
+        circle.setRadius((pane.getHeight()>pane.getWidth())?pane.getWidth()/2:pane.getHeight()/2);
+
+        Text text = new Text(50,50,"Hello world");
+        pane.getChildren().add(circle);
+        pane.getChildren().add(text);
+
+
+        primaryStage.setTitle("Circle");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
+
+
+
